5b51d756c84972f9b27dfd32688f02d890d72570
levi922 <00001108006607sp@al.educacao.sp.gov.br>
2025-11-21 23:15:51 -0300

    Add files via upload

---
+++ aluno.py
@@ -0,0 +1,27 @@
+class Aluno:
+    def __init__(self, nome, RA, nota1, nota2, média):
+        self.nome = nome
+        self.RA = RA
+        self.nota1 = nota1
+        self.nota2 = nota2
+        self.média = (nota1+nota2)/2
+    
+    def ficha_aluno(self):
+        self.valores()
+        self.calculo_média()
+
+    def valores(self):
+        print(f'---- ficha de identificação ----')
+        print(f'nome:{self.nome}')
+        print(f'Registro de aluno:{self.RA}')
+
+    def calculo_média(self):
+        print(f'primeira nota:{self.nota1}')
+        print(f'segunda nota:{self.nota2}')
+        print(f'média:{self.média}')
+
+        
+    pass
+
+FI = Aluno(nome= input('nome:'), RA= input('RA:'), nota1= int(input('sua nota 1:')), nota2= int(input('sua nota2:')), média= '')
+FI.ficha_aluno()
