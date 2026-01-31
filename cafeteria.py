94c2d1906640a2f71773fe8fdd0b02d2ea115616
levi922 <00001108006607sp@al.educacao.sp.gov.br>
2026-01-31 12:04:33 -0300

    Update cafeteria.py
    projeto tirado do nathan :)

--- cafeteria.py
+++ cafeteria.py
@@ -1,9 +1,11 @@
-print('menu da cafeteria')
-dict = {'expresso': '7.00', 'Pingado': '9.00', 'Pão na chapa': '11.00', 'Achocolatado': '13.00', 'Misto quente': '15.00'}
-print(dict.values())
-print(dict.keys())
+cardapio = {"expresso":7.0,  "pingado": 9.0, "pão na chapa":11.0, "achocolatado": 13.0, "misto quente": 15.0}
+precoTotal = 0
 while True:
-    for key in dict:
-        input(key).lower
-        int(input('Quantidade:'))
+    item = input().lower()
+    if(item != ""):
+        qtd = int(input())
+        precoTotal += cardapio[item] * qtd
+    else:
+        break
+print(precoTotal)
         
